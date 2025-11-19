93445b981a40a9de05a041ba9458b9e85876fd9c
Micrikacik <jiriharvalik@seznam.cz>
2025-11-19 18:42:28 +0100

    Update for both DR_BCG and its preconditioned variant
    The algorithms run without errors, but were not yet tested on a greater scale. Comparing the convergence curves to the ones in the paper they have a drop at the beginning - not sure why.

--- matlab/DR_BCG/DR_BCG_exp.m
+++ matlab/DR_BCG/DR_BCG_exp.m
@@ -1,9 +1,9 @@
-function [omega_err, x_k] = Prec_DR_BCG_exp(A, b, x_0, x_ex, L, maxit)
+function [omega_err, x_k] = DR_BCG_exp(A, b, x_0, x_ex, maxit)
 
 x_k = x_0;
 r_k = b - A * x_0;
-[w_k,sig_k] = qr(L \ r_k, 'econ');
-s_k = L' \ w_k;
+[w_k,sig_k] = qr(r_k, 'econ');
+s_k = w_k;
 
 omega_err = zeros(maxit + 1, 1);
 omega_err(1) = omega_error(A,x_0,x_ex);
@@ -12,7 +12,7 @@ for k = 1:maxit
     ksi_k_inv = s_k' * A * s_k;
     x_k = x_k + s_k * (ksi_k_inv \ sig_k);
     omega_err(k + 1) = omega_error(A, x_k, x_ex);
-    [w_k,zeta] = qr(w_k - L \ (A * (s_k / ksi_k_inv)), 'econ');
-    s_k = L' \ w_k + s_k * zeta';
+    [w_k,zeta] = qr(w_k - A * (s_k / ksi_k_inv), 'econ');
+    s_k = w_k + s_k * zeta';
     sig_k = zeta * sig_k;
 end
