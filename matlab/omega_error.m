5fa260e3420fcabb0ff91f2feaf941a1726d881c
Tomáš Medřický <tomas.medricky@fsv.cvut.cz>
2025-12-09 10:43:45 +0100

    Naively refactored omega_k tracking fun (precomputed initial term x'Ax + trace() avoided)

--- matlab/omega_error.m
+++ matlab/omega_error.m
@@ -1,10 +1,24 @@
-function o_k = omega_error(A,x_k,x_ex)
-
-% Function used to calculate the special "A-norm" of the error. This norm
-% is used in the numerical experiments section of the paper.
+function o_k = omega_error(A, x_k, x_ex, omegaInit)
+% OMEGA_ERROR  Generaliized relative A-norm of the error.
 %
-% A is positive definite matrix.
-% x_k is the approximation of x_ex.
+% o_k = omega_error(A, x_k, x_ex, omegaDen)
+%     A        SPD matrix.
+%     x_k      current iterate.
+%     x_ex     reference solution.
+%     omegaDen optional precomputed trace(x_ex' * A * x_ex).
 
-err = x_ex - x_k;
-o_k = sqrt(trace(err' * A * err) / trace(x_ex' * A * x_ex));
+    if nargin < 4 || isempty(omegaInit)
+        omegaInit = trace(x_ex' * A * x_ex);
+        fprintf('Computed omegaInit = %.4e\n', omegaInit);
+        fprintf('Profiling: Consider precomputing and passing as argument for efficiency\n');
+    end
+
+    err = x_ex - x_k;
+    Aerr = A * err;
+    num = sum(err .* Aerr, 'all'); % = trace(err' * A * err)
+    o_k = sqrt(num / omegaInit);
+
+    % Alternative (slower) implementation:
+        % err = x_ex - x_k;
+        % o_k = sqrt(trace(err' * A * err) / trace(x_ex' * A * x_ex));
+end
