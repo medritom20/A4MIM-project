0dde9c2498b0622b439cacda8454f7f6264aae6f
moschehaus <belankkamil@gmail.com>
2025-12-15 23:44:33 +0100

    comparison of bf_bcg and pdp_bcg, bf_bcg update

--- matlab/BF_BCG/bf_bcg.m
+++ matlab/BF_BCG/bf_bcg.m
@@ -1,120 +1,129 @@
-function [Xblk, relres, iter, reshist] = bf_bcg(A, B, tol, maxit)
-%BF_BCG  BF-Omin / BF-ECG (Algorithm 2.3) 
-%
-%   [Xblk, relres, iter, reshist] = bf_ebg(A, B, tol, maxit)
-%
-%   A      : SPD matrix (n x n)
-%   B      : the RHS (n x m)
-%   tol    : stopping tolerance on ||sum(R_k)|| / ||sum(R_0)||
-%   maxit  : maximum number of iterations
-%
-%   Outputs:
-%     Xblk   : n x m block of accumulated solution updates (X_k)
-%              The scalar solution is x = sum(Xblk, 2).
-%     relres : final RELATIVE residual
-%     iter   : number of iterations performed
-%     reshist: history of ||r_k|| / ||r_0|| (length iter+1)
-%--------------------------------------------------------------%
+function [Xblk, omega_final, iter, omegahist] = bf_bcg(A, B, applyM, X_ex, maxit, tol, thr)
+	%BF_BCG  BF-Omin / BF-ECG (Algorithm 2.3) 
+	%
+	%   [Xblk, omega_final, iter, omegahist] = bf_bcg(A, B, X_ex, maxit, tol, thr)
+	%
+	%   A      : SPD matrix (n x n)
+	%   B      : the RHS (n x m)
+	%   applyM : the preconditioner as a function handle
+	%   X_ex   : the exact solution to the problem
+	%   maxit  : maximum number of iterations
+	%   tol    : stopping tolerance on \omega
+	%   thr    : threshold of "almost linear dependence"
+	%
+	%   Outputs:
+	%     Xblk         : n x m block of accumulated solution updates (X_k)
+	%              The scalar solution is x = sum(Xblk, 2).
+	%     omega_final  : final "A-residual"
+	%     iter         : number of iterations performed
+	%     omegahist    : history of \omega_k (length iter+1)
+	% Solves the block system AX = B using the break free conjugate gradient method; no preconditioning, the initial point is zero.
+	%--------------------------------------------------------------%
 
 
-% Initial residual block and search directions are the block RHS
-R = B;                 % R_0 (n x t)
-Z = B;                 % Z_1 = R_0
+	% initial residual block and search directions are the block RHS
+	R = B;                 % R_0 (n x t)
+	Z = applyM(R);         % Z_1 = R_0
 
-[n, t] = size(R);
+	[n, t] = size(R);
 
-% If the RHS is zero, the solution must be zero
-r0_vec = sum(R, 2);    % r_0
-normb  = norm(r0_vec);
+	% if the RHS is zero, the solution must be zero
+	r0_vec = sum(R, 2);    % r_0
+	normb  = norm(r0_vec);
 
-if normb == 0
-    Xblk    = zeros(n, t);
-    relres  = 0;
-    iter    = 0;
-    reshist = 0;
-    return;
-end
+	if normb == 0
+		Xblk    = zeros(n, t)
+		omega_final  = 0;
+		iter    = 0;
+		omegahist = 0;
+		return;
+	end
 
-% Initialisation of the arrays
-reshist       = zeros(maxit+1, 1);
-reshist(1)    = 1.0;   % ||r_0|| / ||r_0|| = 1
-Xblk          = zeros(n, t);
+	% initialisation of the arrays
+	omegahist       = zeros(maxit+1, 1);
+	omegahist(1)    = 1.0;   
+	Xblk          = zeros(n, t);
+	fprintf('BF-BCG: it.%3d\trrank = %2d\tomega = %.2e\n', 0, size(Z,2), omegahist(1));
 
-for k = 1:maxit
 
-    tcur = size(Z, 2);
-    if tcur == 0
-        warning('bf_ecg:allDropped', ...
-                'All search directions dropped at iteration %d.', k);
-        break;
-    end
+	fprintf('it  tcur  rrank   ||R||        ||Z||       omega     alpha_norm\n');
+	for k = 1:maxit
+		tcur = size(Z, 2);
+		if tcur == 0
+			warning('bf_ecg:allDropped', ...
+				'All search directions dropped at iteration %d.', k);
+			break;
+		end
 
-    % ----------------------------------------
-    % Step 3: P_k = Z_k (Z_k^T A Z_k)^(-1/2)
-    % C = Z_k^T A Z_K is a SPD matrix, so C^-1 is SPD and C^_1 = C^(-1/2) (C^(-1/2))^T, meaning we can compute the Cholesky factorization of C  = L L^T and C^(-1/2) = L^-T
+		% step 3: P_k = Z_k (Z_k^T A Z_k)^(-1/2)
+		% C = Z_k^T A Z_K is a SPD matrix, so C^-1 is SPD and C^_1 = C^(-1/2) (C^(-1/2))^T, meaning we can compute the Cholesky factorization of C  = L L^T and C^(-1/2) = L^-T
 
-    % ----------------------------------------
-    AZ = A * Z;                % n x tcur; we compute this only once
-    C  = Z' * AZ;              % tcur x tcur
+		AZ = A * Z;                % n x tcur; we compute this only once
+		C  = Z' * AZ;              % tcur x tcur
 
-    % Cholesky: C = L * L^T
-    L = chol(C, 'lower');      % assume A is SPD and basis is good enough
+		% Cholesky: C = L * L^T
+		L = chol(C, 'lower');      % assume A is SPD and basis is good enough
 
-    % P_k and A P_k
-    P  = Z / L';
-    AP = AZ / L';
+		% P_k and A P_k
+		P  = Z / L';
+		AP = AZ / L';
 
-    % ----------------------------------------
-    % Step 4: alpha_k = P_k^T R_{k-1}
-    % ----------------------------------------
-    alpha = P' * R;            % tcur x t
+		% step 4: alpha_k = P_k^T R_{k-1}
+		alpha = P' * R;            % tcur x t
 
-    % ----------------------------------------
-    % Steps 5–6:
-    % X_k = X_{k-1} + P_k alpha_k
-    % R_k = R_{k-1} - A P_k alpha_k
-    % ----------------------------------------
-    Xblk = Xblk + P * alpha;
-    R    = R    - AP * alpha;
+		% steps 5–6:
+		% X_k = X_{k-1} + P_k alpha_k
+		% R_k = R_{k-1} - A P_k alpha_k
+		Xblk = Xblk + P * alpha;
+		R    = R    - AP * alpha;
 
-    % Update the residuum arrays, compute the norm of the combined residuum
-    r_vec  = sum(R, 2);
-    relres = norm(r_vec) / normb;
-    reshist(k+1) = relres;
+		% update the residuum arrays, compute the norm of the combined residuum
+		err = X_ex - Xblk; 
+		omega = sqrt(trace(err' * A * err) / trace(X_ex' * A * X_ex));
+		omegahist(k+1) = omega;
 
-    if relres < tol
-        break;
-    end
+		if omega < tol
+			break;
+		end
 
-    % ----------------------------------------
-    % Steps 10–11: Orthomin update
-    %   beta_k = (A P_k)^T R_k
-    %   Z_{k+1} = R_k - P_k beta_k
-    % ----------------------------------------
-    beta = AP' * R;            % tcur x t
-    Znew = R - P * beta;       % n x t
+		% steps 10–11: orthomin update
+		%   beta_k = (A P_k)^T R_k
+		%   Z_{k+1} = R_k - P_k beta_k
+		beta = AP' * R;            % tcur x t
+		Znew = R - P * beta;       % n x t
+		Znew = applyM(Znew);
 
-    % ----------------------------------------
-    % Step 12: RRQR(Z_{k+1}, sqrt(eps))
-    %   (simple rank-revealing QR via MATLAB qr)
-    % ----------------------------------------
-    [Q, Rq] = qr(Znew, 0);     % Znew = Q * Rq
-    diagR   = abs(diag(Rq)); %the magnitude of the diagonal elements of the upper triangular matrix Rq
+		% step 12: SVD based rank truncation
+		%   (rank-revealing SVD done via MATLAB svd)
+		[U, S, ~] = svd(Znew,0);
+		svs = diag(S);
+		fprintf('it=%3d  min SV = %.2e, max SV = %.2e, rrank=%d\n', k, min(svs), max(svs), rrank);
+		relSV = svs/max(svs); % the paper computes relative singular values
+		relSV = svs / max(svs);
+		fprintf('rel min SV = %.2e, max rel SV = %.2e\n', min(relSV), max(relSV));
+		rrank = sum(relSV > thr); %the rank of upper triangular matrix is the number of nonzero diagonal elements 
+		Z = U(:,1:rrank); % Z_{k+1}; this assures Z has linearly independent columns
 
-    thr   = 1e-12 * max(diagR);    % this is our definition of "almost linearly dependent"
-	    rrank = sum(diagR > thr); %the rank of upper triangular matrix is the number of nonzero diagonal elements 
+		% for diagnostics of stalling, we print plenty of information
+		normR = norm(R, 'fro');
+		normZ = norm(Znew, 'fro');
+		alpha_norm = norm(alpha, 'fro');
+		fprintf('%3d  %3d   %3d   %.2e   %.2e   %.2e   %.2e\n', ...
+			k, tcur, rrank, normR, normZ, omega, alpha_norm);
 
-    if rrank == 0
-        warning('bf_ecg:allDroppedRRQR', ...
-                'All directions dropped by RRQR at iteration %d.', k);
-        break;
-    end
+		fprintf('%3d  %3d   %3d   %.2e   %.2e   %.2e   %.2e\n', ...
+			k, tcur, rrank, normR, normZ, omega, alpha_norm);
 
-    Z = Q(:, 1:rrank);         % Z_{k+1}; this assures Z has linearly independent columns
-end
+		if rrank == 0
+			warning('bf_bcg:allDroppedRRQR', ...
+				'All directions dropped by RRQR at iteration %d.', k);
+			break;
+		end
+	end
 
-iter    = k;
-reshist = reshist(1:iter+1);
+	iter    = k;
+	omegahist = omegahist(1:iter+1);
+	omega_final = omegahist(end);
 
 end
 
---
+++ matlab/BF_BCG/bf_dp.m
@@ -0,0 +1,101 @@
+%%%%%%%%%%%%%%%%
+% directory management
+%%%%%%%%%%%%%%%%
+
+srcDir = fullfile(pwd, "..");
+bfDir = fullfile(srcDir, "BF_BCG");
+pdpDir = fullfile(srcDir, "PDP_BCG");
+dataDir = fullfile(srcDir, "data", "s3dkt3m2.mat");
+outDir = fullfile(srcDir, "out", "results", "dp_bf");
+addpath(srcDir);
+addpath(bfDir);
+addpath(pdpDir);
+
+%%%%%%%%%%%%%%%%
+% preconditioner options
+%%%%%%%%%%%%%%%%
+
+opts.type = 'ict';        % threshold dropping
+opts.droptol = 1e-5;      % drop tolerance
+opts.diagcomp = 1e-2;     % global diagonal shift
+
+L = ichol(A, opts);       
+applyM = @(R) L' \ (L \ R);
+
+
+% loading the data
+A = load(dataDir).Problem.A;
+n = length(A);
+
+% iterations, tolerances
+maxit = 500;
+tol = 1e-8;
+thrs = [1e-7, 1e-8, 1e-9, 1e-10];
+linespecs = ["r:", "r-.", "r--", "r-"];
+
+%%%%%%%%%%%%%%%%
+%m = 16
+%%%%%%%%%%%%%%%%
+
+m1 = 16;
+X_ex1 = rand(n,m1);
+b1 = A*X_ex1;
+X_zero1 = zeros(n,m1);
+
+fig16 = figure;
+ax16 = axes(fig16);
+hold(ax16, 'on');
+
+for i=1:length(thrs)
+	thr = thrs(i);
+	linespec = linespecs(i);
+	[~, ~, ~, omegahist] = bf_bcg(A, b1, applyM, X_ex1, maxit, tol, thr);
+	semilogy(ax16, omegahist, linespec);
+end
+
+[~, omegahist] = PDPBCG(A, b1, X_zero1, applyM, X_ex1, maxit, tol);
+semilogy(omegahist, 'b-');
+set(ax16, 'YScale', 'log'); % this should be redundant
+xlabel(ax16, "iteration"); ylabel(ax16, "\omega_k");
+ylim(ax16,[1e-6,1.0])
+legend(ax16,[string(thrs), "DP-BCG"] , 'Location', 'best');
+grid(ax16, 'on');
+hold(ax16, 'on');
+
+figPath = fullfile(outDir, "dp_bf16.fig")
+pdfPath = fullfile(outDir, "dp_bf16.pdf")
+savefig(fig16, figPath);
+exportgraphics(fig16, pdfPath, 'ContentType', 'vector');
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%m = 64
+%%%%%%%%%%%%%%%%%%%%%%%%
+m2 = 64;
+X_ex2 = rand(n,m2);
+b2 = A*X_ex2;
+X_zero2 = zeros(n,m2);
+fig64 = figure;
+ax64 = axes(fig64);
+hold(ax64,'on');
+
+for i = 1:length(thrs)
+	thr = thrs(i);
+	linespec = linespecs(i);
+	[~, ~, ~, omegahist] = bf_bcg(A, b2, applyM, X_ex2, maxit, tol, thr);
+	semilogy(omegahist, linespec);
+end
+
+[~, omegahist] = PDPBCG(A, b2, X_zero2, applyM, X_ex2, maxit, tol);
+semilogy(omegahist, 'b-');
+set(ax64, 'YScale', 'log'); %this should be redundant
+xlabel(ax64, "iteration"); ylabel(ax64, "\omega_k");
+ylim(ax64,[1e-6,1.0]);
+legend(ax64,[string(thrs), "DP-BCG"] , 'Location', 'best');
+grid(ax64, 'on');
+hold(ax64,'on');
+
+figPath = fullfile(outDir, "dp_bf64.fig")
+pdfPath = fullfile(outDir, "dp_bf64.pdf")
+savefig(fig64,figPath);
+exportgraphics(fig64, pdfPath, 'ContentType', 'vector');
+
