40be0d7799dbbb95fb36e3041b7cba02b82c6e01
Tomáš Medřický <tmedritom@gmail.com>
2025-12-16 12:03:00 +0100

    PDP-BCG driver update: test experiment #3

--- matlab/methods/PDP_BCG/drive_pdpbcgr.m
+++ matlab/methods/PDP_BCG/drive_pdpbcgr.m
@@ -1,25 +1,25 @@
 clearvars; clc; close all
 addpath(genpath('..\'))
 
-dataFile = 'bcsstk03.mat';              % 's3dkt3m2.mat' | 'bcsstk03.mat'
+dataFile = 's3dkt3m2.mat';              % 's3dkt3m2.mat' | 'bcsstk03.mat'
 [A, ~] = load_problem(dataFile);
 n = size(A, 1);
 
-nRHS = 4;                                       % 1 | 2 | 4 | 6 | ...
-Xtrue = randn(n, nRHS);
-B = A * Xtrue;
+nRHS = 16;                                       % 1 | 2 | 4 | 6 | ...
+B = randn(n, nRHS);
+Xtrue = A \ B;
 X0 = zeros(n, nRHS);
 
+% prec.type = 'none';
 prec.type = 'ichol';                            % 'ichol' | 'none'
 prec.opts.type = 'ict';
 prec.opts.droptol = 1e-5;
 prec.opts.diagcomp = 1e-2;
 applyM = build_preconditioner(A, prec);         % @(u) M^{-1}u
 
-maxIt = 100;
-tol = 1e-8;
+maxIt = 500;
+tol = 1e-14;
 
 [X, omegaHist] = PDPBCG(A, B, X0, applyM, Xtrue, maxIt, tol);
 
 plot_convergence(omegaHist, get_legend_label(prec) );
-
